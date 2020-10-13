d6a91e79975ea63cb78130ee8b43477c74c78c0b
Vraj Mehta <vrajjmehta@gmail.com>
2020-10-13 17:07:45 +1100

    GCP Deploy

---
+++ public/build/precache-manifest.c535ac5846cbec0113af893edf88958c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dd8a25585e8d98c83e9dfc69b3d1384",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c51970b5f14ef0a3d8b3",
+    "url": "/static/css/2.847e01e0.chunk.css"
+  },
+  {
+    "revision": "040967991021fd49a4f1",
+    "url": "/static/css/main.2029fefc.chunk.css"
+  },
+  {
+    "revision": "c51970b5f14ef0a3d8b3",
+    "url": "/static/js/2.e92e963d.chunk.js"
+  },
+  {
+    "revision": "f576af3254df0679363d3e6ffe804e36",
+    "url": "/static/js/2.e92e963d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "040967991021fd49a4f1",
+    "url": "/static/js/main.4011e129.chunk.js"
+  },
+  {
+    "revision": "11d3c6cea24bb7d9b072",
+    "url": "/static/js/runtime-main.054d6569.js"
+  }
+]);
--- server/index.js
+++ server/index.js
@@ -1,1 +1,14 @@
 const express = require('express');
+let bodyParser = require('body-parser');
+const app = express();
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public/build'));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log('Server is up on port ' + PORT);
+});
