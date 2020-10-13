ebfef903e164a1caf02a9c390f1537effa145ca6
Vraj Mehta <vrajjmehta@gmail.com>
2020-10-13 17:08:41 +1100

    Revert "GCP Deploy"
    This reverts commit d6a91e79975ea63cb78130ee8b43477c74c78c0b.

--- server/index.js
+++ server/index.js
@@ -1,14 +1,1 @@
 const express = require('express');
-let bodyParser = require('body-parser');
-const app = express();
-
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(__dirname + '/public/build'));
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-    console.log('Server is up on port ' + PORT);
-});
