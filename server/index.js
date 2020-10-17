7f6529255ff9e1568e0a4c715f4ee319abca0d15
Declan200 <declan2317@hotmail.com>
2020-10-17 12:12:40 +1100

    back-end server and GCP upload feature

--- server/index.js
+++ server/index.js
@@ -1,1 +1,18 @@
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser')
+const app = express();
+
+var corsOptions = {
+    origin: "http://localhost:3000"
+};
+
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log('Server is up on port ' + PORT);
+});
