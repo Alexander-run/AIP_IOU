89437d2c87fb228fb374cd0748a0a3e972c33745
Vraj Mehta <vrajjmehta@gmail.com>
2020-10-19 16:40:36 +1100

    File Upload function

--- server/index.js
+++ server/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser')
 const app = express();
+const path = require('path');
+const debug = require('debug')('myapp:server');
+const multer = require('multer');
 
 var corsOptions = {
     origin: "http://localhost:3000"
@@ -12,17 +15,38 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 5000;
+app.use(express.static(path.join(__dirname, './uploads')))
 
 app.listen(PORT, () => {
     console.log('Server is up on port ' + PORT);
 });
 
-app.get('/express_backend', (req, res) => {
-    res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
+// Disk storage for files
+var storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, __dirname + '/uploads')
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname)
+    }
 });
 
+// Mutler object upload
+const upload = multer({ storage: storage });
+
+// store file uploads
+app.post('/upload', upload.single('file'), (req,res) => {
+    debug(req.file);
+    console.log('storage location is ', req.hostname +'/' + req.file.path);
+    return res.send(req.file);
+});
+
+app.get('/check', (req,res) => {
+    res.sendFile(path.join(__dirname,'upload.html'))
+})
+
 // Production setup
-// app.use(express.static(path.join(__dirname, 'build')))
+// app.use(express.static(path.join(__dirname, '../build')))
 
 // app.get('/', (req, res) => {
 //     res.sendFile(path.join(__dirname, 'build', 'index.html'))
